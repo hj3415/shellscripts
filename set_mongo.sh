85303c1c4d212a71a3112bb85bb47af039bdb009
hyungjin kim <hj3415@gmail.com>
2024-08-10 11:12:10 +0900

    t

--- set_mongo.sh
+++ set_mongo.sh
@@ -9,8 +9,6 @@ echo "***********************************************************************"
 ID="hj3415"
 PASS="piyrw421"
 PORT="27017"
-EXPRESS_PORT="8081"
-DATA_PATH="${HOME}/mongo_data"
 
 MYIP=`hostname -I | cut -d ' ' -f1`
 
@@ -21,71 +19,65 @@ sudo apt install -y docker-compose-plugin
 docker compose version
 
 # 필요한 기본 디렉토리 생성
-echo ">>> Do you want to reset mongodb directory?(${DATA_PATH}) (y/N)"
-read answer
-if [[ ${answer} == 'y' ]];then
-sudo rm -rf ${DATA_PATH}
-fi
-mkdir -p ${DATA_PATH}
-
 rm -rf setup_mongo
 mkdir setup_mongo; cd $_
 
+tee mongod.conf<<EOF
+storage:
+  dbPath: /data/db
+
+systemLog:
+  destination: file
+  logAppend: true
+  path: /var/log/mongodb/mongod.log
+
+
+net:
+  bindIp: 0.0.0.0
+  port: ${PORT}
+
+processManagement:
+  timeZoneInfo: /usr/share/zoneinfo
+
+security:
+  authorization: enabled
+EOF
+
 tee docker-compose.yml<<EOF
 # Use root/example as user/password credentials
-version: '3'
-
 services:
-
   mongo:
-    image: mongo
-    container_name: mongo
+    image: mongo:latest
+    container_name: mongodb
     ports:
-      - ${PORT}:27107
+      - "${PORT}:27107"
     volumes:
-      - ${DATA_PATH}:/data
+      - mongo-data:/data/db
+      - ./mongod.conf:/etc/mongod.conf
     restart: always
     environment:
       MONGO_INITDB_ROOT_USERNAME: ${ID}
       MONGO_INITDB_ROOT_PASSWORD: ${PASS}
+    command: ["mongod", "--config", "/etc/mongod.conf"]
+    network_mode: host
 
-  mongo-express:
-    image: mongo-express
-    container_name: mongo-express
-    restart: always
-    ports:
-      - ${EXPRESS_PORT}:8081
-    environment:
-      ME_CONFIG_BASICAUTH_USERNAME: ${ID}
-      ME_CONFIG_BASICAUTH_PASSWORD: ${PASS}
-      ME_CONFIG_MONGODB_ADMINUSERNAME: ${ID}
-      ME_CONFIG_MONGODB_ADMINPASSWORD: ${PASS}
-      ME_CONFIG_MONGODB_URL: mongodb://${ID}:${PASS}@mongo:${PORT}/
-networks:
-  default:
-    name: mongodb_network
+volumes:
+  mongo-data:
+    external: true
 EOF
 
 # 이미지 최신버전으로 교체
 docker stop mongo
-docker stop mongo-express
-
 docker rm mongo
-docker rm mongo-express
-
-# 이미지 다시 생성
 docker pull mongo
-docker pull mongo-express
 
+docker volume create mongo-data
 docker compose up -d
 cd ..
 
 # Open firewall
 sudo ufw allow ${PORT}
-sudo ufw allow ${EXPRESS_PORT}
 
 bash ./tools/making_motd.sh mongo \
-  "web access by mongo_express - http://${MYIP}:${EXPRESS_PORT}" \
-  "ID - ${ID}, PASS - ${PASS}" \
-  "원격 접속은 불가능함." \
+  "addr - mongodb://${ID}:${PASS}@${MYIP}:${PORT}/" \
   "data - ${DATA_PATH}"
