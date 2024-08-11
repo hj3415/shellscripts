61f34a29c5b6614651e5d2edfbd97ef6c6aea1b6
hyungjin kim <hj3415@gmail.com>
2024-08-11 10:21:07 +0900

    Update 175automation.sh

--- 175automation.sh
+++ 175automation.sh
@@ -16,19 +16,40 @@ chk_job() {
 
 NFS_PATH=$(which nfs)
 
-
 # 월-금 9-15시까지 매 10분단위로 갱신
 # 문자열 글로빙 문제로 작은따옴표와 큰따옴표를 적절히 사용해야 에러가 나지 않는다.
 C101='*/10 9-15 * * 1-5 '"$NFS_PATH"' c101 all'
 
+# 월-금 14시에 마지막 갱신하고 노티
+C101_FINAL='0 16 * * 1-5 '"$NFS_PATH"' c101 all --noti'
+
 # 월-금 매일 18시에 갱신
-C108='0 18 * * 1-5 '"$NFS_PATH"' c108 all'
+C108='0 18 * * 1-5 '"$NFS_PATH"' c108 all --noti'
+
+# 토 06시에 갱신
+C106='0 6 * * 6 '"$NFS_PATH"' c106 all --noti'
+
+# 토 11시에 갱신
+C103Y='0 11 * * 6 '"$NFS_PATH"' c103y all --noti'
+
+# 토 17시에 갱신
+C103Q='0 17 * * 6 '"$NFS_PATH"' c103q all --noti'
+
+# 일 09시에 갱신
+C104Y='0 09 * * 7 '"$NFS_PATH"' c104y all --noti'
+
+# 일 17시에 갱신
+C104Q='0 17 * * 7 '"$NFS_PATH"' c104q all --noti'
 
 
 chk_job "$C101"
+chk_job "$C101_FINAL"
 chk_job "$C108"
+chk_job "$C106"
+
+chk_job "$C103Y"
+chk_job "$C103Q"
+chk_job "$C104Y"
+chk_job "$C104Q"
 
 
-
-
-#['c101', 'c106', 'c103y', 'c103q', 'c104y', 'c104q', 'c108', 'all_spider']
