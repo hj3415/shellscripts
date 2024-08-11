805a0b2dee7d532a7fc18ea4940013e869b67879
hyungjin kim <hj3415@gmail.com>
2024-08-11 09:03:27 +0900

    Update 175automation.sh

--- 175automation.sh
+++ 175automation.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
 chk_job() {
-	# 현재 사용자의 crontab에서 해당 작업이 이미 존재하는지 확인
-	crontab -l | grep -F "$1" > /dev/null
+	echo "$1"
+    # 현재 사용자의 crontab에서 해당 작업이 이미 존재하는지 확인
+    crontab -l | grep -F "$1" > /dev/null
 
-	if [ $? -eq 0 ]; then
-	    echo "The job is already in crontab."
-	else
-	    # 작업이 없을 경우에만 추가
-	    (crontab -l; echo "$1") | crontab -
-	    echo "New job added to crontab."
-	fi
+    if [ $? -eq 0 ]; then
+        echo "The job is already in crontab."
+    else
+        # 작업이 없을 경우에만 추가
+        (crontab -l; echo "$1") | crontab -
+        echo "New job added to crontab."
+    fi
 }
 
-NFS_PATH = $(which nfs)
-
-# 매일 9-15시까지 매 10분단위로 갱신
-C101 = "*/10 9-15 * * * $NFS_PATH c101 all"
+NFS_PATH=$(which nfs)
+#echo $NFS_PATH
 
 
-chk_job $C101
+# 월-금 9-15시까지 매 10분단위로 갱신
+# 문자열 글로빙 문제로 작은따옴표와 큰따옴표를 적절히 사용해야 에러가 나지 않는다.
+C101='*/10 9-15 * * 1-5 '"$NFS_PATH"' c101 all'
+#echo "$C101"
+
+chk_job "$C101"
 
 
 
